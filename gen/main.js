b89fc12750347a1a4af168e904d9d92c8d038900
alanBMC <alanbmc00@gmail.com>
2024-07-24 15:33:19 -0400

    feat: criando estrutura do projeto

---
+++ gen/main.js
@@ -0,0 +1,65 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron')
+const fs = require('fs')
+const path = require('path')
+const { parseStringPromise } = require('xml2js')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+ipcMain.handle('read-data', async () => {
+  try {
+    const data = fs.readFileSync('data.json', 'utf8')
+    return data ? JSON.parse(data) : []
+  } catch (error) {
+    console.error('Error reading data:', error)
+    return []
+  }
+})
+
+ipcMain.handle('write-data', async (event, data) => {
+  try {
+    fs.writeFileSync('data.json', JSON.stringify(data, null, 2))
+    return true
+  } catch (error) {
+    console.error('Error writing data:', error)
+    return false
+  }
+})
+
+ipcMain.handle('select-xml-file', async () => {
+  const result = await dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [{ name: 'XML Files', extensions: ['xml'] }]
+  })
+
+  if (result.canceled) {
+    return null
+  } else {
+    const filePath = result.filePaths[0]
+    const xmlData = fs.readFileSync(filePath, 'utf8')
+    return parseStringPromise(xmlData)
+  }
+})
